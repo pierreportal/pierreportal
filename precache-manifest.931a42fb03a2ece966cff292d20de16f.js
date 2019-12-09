d3d5738a7e52d082d5451831fe6886b82ef63d4d
pierre portal <pierreportal.mac@gmail.com>
2019-12-09 14:25:37 +0100

    Updates

---
+++ precache-manifest.931a42fb03a2ece966cff292d20de16f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3236f64aa49923b9085449311451120",
+    "url": "/pierreportal/index.html"
+  },
+  {
+    "revision": "20ca0d92cf201bae2824",
+    "url": "/pierreportal/static/css/main.35f1454e.chunk.css"
+  },
+  {
+    "revision": "5d87a4847bd4ccf802c7",
+    "url": "/pierreportal/static/js/2.e8461a09.chunk.js"
+  },
+  {
+    "revision": "20ca0d92cf201bae2824",
+    "url": "/pierreportal/static/js/main.2e2f6185.chunk.js"
+  },
+  {
+    "revision": "97d1eccfb2141eec7345",
+    "url": "/pierreportal/static/js/runtime-main.5fd44b67.js"
+  },
+  {
+    "revision": "7593eddd2368a438e0ab04f8bfd2dea6",
+    "url": "/pierreportal/static/media/me.7593eddd.png"
+  }
+]);
